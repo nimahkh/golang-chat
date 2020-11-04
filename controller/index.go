6a8622efcc9a30d870029a4394b2c14729977f60
Nima Habibkhoda <nima.habibkhoda@ttgint.com>
2020-11-04 14:41:29 +0300

    chage structure

---
+++ controller/index.go
@@ -0,0 +1,54 @@
+package controller
+
+import (
+	m "gin/chat/manager"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+)
+
+func Stream(c *gin.Context, rm *m.Manager) {
+	roomid := c.Param("roomid")
+	listener := rm.OpenListener(roomid)
+
+	defer rm.CloseListener(roomid, listener)
+
+	clientGone := c.Writer.CloseNotify()
+	c.Stream(func(w io.Writer) bool {
+		select {
+		case <-clientGone:
+			return false
+		case message := <-listener:
+			c.SSEvent("message", message)
+			return true
+		}
+	})
+}
+
+func RoomGET(c *gin.Context) {
+	roomid := c.Param("roomid")
+	userid := c.Param("user")
+	isme := c.Param("user")
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"roomid": roomid,
+		"userid": userid,
+		"isme":   isme,
+	})
+}
+
+func RoomPOST(c *gin.Context, rm *m.Manager) {
+	roomid := c.Param("roomid")
+	userid := c.PostForm("user")
+	message := c.PostForm("message")
+	rm.Submit(userid, roomid, message)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  "success",
+		"message": message,
+	})
+}
+
+func RoomDELETE(c *gin.Context, rm *m.Manager) {
+	roomid := c.Param("roomid")
+	rm.DeleteBroadcast(roomid)
+}
--- main.go
+++ main.go
@@ -1,70 +1,32 @@
 package main
 
 import (
+	"gin/chat/controller"
+	m "gin/chat/manager"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
-	"io"
-	"net/http"
 )
 
-var roomManager *Manager
+var roomManager *m.Manager
 
 func main() {
-	roomManager = NewRoomManager()
+	roomManager = m.NewRoomManager()
 	router := gin.Default()
-	//router.SetHTMLTemplate(Html)
 	router.LoadHTMLGlob("template/*")
 	router.Use(static.Serve("/assets", static.LocalFile("./assets", true)))
 
-	router.GET("/room/:roomid/:user", roomGET)
-	router.POST("/room/:roomid/:user", roomPOST)
-	router.DELETE("/room/:roomid/:user", roomDELETE)
-	router.GET("/stream/:roomid/:user", stream)
-
-	router.Run()
-}
-
-func stream(c *gin.Context) {
-	roomid := c.Param("roomid")
-	listener := roomManager.OpenListener(roomid)
-	defer roomManager.CloseListener(roomid, listener)
-
-	clientGone := c.Writer.CloseNotify()
-	c.Stream(func(w io.Writer) bool {
-		select {
-		case <-clientGone:
-			return false
-		case message := <-listener:
-			c.SSEvent("message", message)
-			return true
-		}
+	router.GET("/room/:roomid/:user", func(context *gin.Context) {
+		controller.RoomGET(context)
 	})
-}
-
-func roomGET(c *gin.Context) {
-	roomid := c.Param("roomid")
-	userid := c.Param("user")
-	isme := c.Param("user")
-	c.HTML(http.StatusOK, "index.html", gin.H{
-		"roomid": roomid,
-		"userid": userid,
-		"isme":   isme,
+	router.POST("/room/:roomid/:user", func(context *gin.Context) {
+		controller.RoomPOST(context, roomManager)
 	})
-}
-
-func roomPOST(c *gin.Context) {
-	roomid := c.Param("roomid")
-	userid := c.PostForm("user")
-	message := c.PostForm("message")
-	roomManager.Submit(userid, roomid, message)
-
-	c.JSON(http.StatusOK, gin.H{
-		"status":  "success",
-		"message": message,
+	router.DELETE("/room/:roomid/:user", func(context *gin.Context) {
+		controller.RoomDELETE(context, roomManager)
 	})
-}
+	router.GET("/stream/:roomid/:user", func(context *gin.Context) {
+		controller.Stream(context, roomManager)
+	})
 
-func roomDELETE(c *gin.Context) {
-	roomid := c.Param("roomid")
-	roomManager.DeleteBroadcast(roomid)
+	router.Run(":8082")
 }
